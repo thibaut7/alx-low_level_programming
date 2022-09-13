b0fb78874a3f128d029d172b11d9c51708303bf3
thibaut7 <thibautmcdonald@gmail.com>
2022-09-13 09:46:43 -0700

    thibaut7

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,61 @@
+/*                                                                                                                                      
+ *
+ *                                                                                                                                       *File: o.posiive                                                                                                                       
+ *
+ *                                                                                                                                        *author: thibaut7                                                                                                                      
+ *
+ *                                                                                                                                        */                                                                                                                                      
+
+#include <stdio.h>                                                                                                                      
+
+#include <stdlib.h>                                                                                                                     
+
+#include <time.h>                                                                                                                       
+
+                                                                                                                                        
+
+/* more headers goes there */                                                                                                           
+
+                                                                                                                                        
+
+/**                                                                                                                                     
+ *
+ *  * main - main function                                                                                                                 
+ *
+ *   *                                                                                                                                      
+ *
+ *    * Return: text                                                                                                                         
+ *
+ *    */                                                                                                                                      
+
+int main(void)                                                                                                                          
+
+	                                                                                                                                        
+
+{                                                                                                                                       
+
+	        int n;                                                                                                                          
+
+		                                                                                                                                        
+
+		        srand(time(0));                                                                                                                 
+
+			        n = rand() - RAND_MAX / 2;                                                                                                      
+
+				        /* your code goes there */                                                                                                      
+
+				        if (n > 0)                                                                                                                      
+
+						                printf("%d is positive\n", n);                                                                                          
+
+					        else if (n == 0)                                                                                                                
+
+							                printf("%d is zero\n", n);                                                                                              
+
+						        else                                                                                                                            
+
+								                printf("%d is negative\n", n);                                                                                          
+
+							        return (0);                                                                                                                     
+
+
