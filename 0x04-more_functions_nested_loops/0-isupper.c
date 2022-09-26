3665e9cdba44a6e8dd2375ccd4cabb5a41e64e88
thibaut7 <thibautmcdonald@gmail.com>
2022-09-26 06:34:57 -0700

    thibaut7

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -9,17 +9,8 @@ int _isupper(int c)
 {
 	if (c>=65 && c<=90)
 	{
-		_putchar(c);
-		_putchar(:);
-		_putchar(' ');
-		_putchar(1);
+		return(1);
 	}
 	else
-	{
-		_putchar(c);
-		_putchar(:);
-		_putchar(' ');
-		_putchar(0);
-	}
-	return(0);
+		return(0);
 }
