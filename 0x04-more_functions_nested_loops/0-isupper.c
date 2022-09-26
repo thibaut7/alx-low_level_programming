05549b7ae81c2086843d27e83d3d3a0ec842d6e2
thibaut <86150292+thibaut7@users.noreply.github.com>
2022-09-26 17:10:59 +0000

    Update 0-isupper.c
    thibaut7

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,13 +2,14 @@
 #include "main.h"
 /**
  * _isupper - check if is uppper
+ *@c: The character to be checked.
  *
  * Return: 0
  */
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-		return(1);
+		return (1);
 	else
-		return(0);
+		return (0);
 }
