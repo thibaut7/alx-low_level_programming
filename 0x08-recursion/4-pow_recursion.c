d92fda837920168f0528a4d29bbb4f0493e1cf82
thibaut <86150292+thibaut7@users.noreply.github.com>
2022-09-28 23:20:05 +0000

    Update 4-pow_recursion.c
    thibaut7

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,9 +1,4 @@
-/*
- * File: 4-pow_recursion.c
- * Auth: Brennan D Baraban
- */
-
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _pow_recursion - Returns the value of x raised to the power of y.
