0900f1535afd81aef2f807503438d53053b74d60
thibaut <86150292+thibaut7@users.noreply.github.com>
2022-09-28 23:39:50 +0000

    Update 1-print_rev_recursion.c
    thibaut7

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,17 +1,7 @@
 #include "main.h"
-
 /**
  * _print_rev_recursion - Prints a string in reverse.
  * @s: The string to be printed.
- */
- #include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
  */
 void _print_rev_recursion(char *s)
 {
