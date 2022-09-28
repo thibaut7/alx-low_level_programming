9a102815a1d7f886dd86a70c9a5195e85695162e
thibaut <86150292+thibaut7@users.noreply.github.com>
2022-09-28 23:14:01 +0000

    thibaut7

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,39 @@
+/*
+ * File: 1-print_rev_recursion.c
+ * Auth: Brennan D Baraban
+ */
+
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - Prints a string in reverse.
+ * @s: The string to be printed.
+ */
+ #include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+void _print_rev_recursion(char *s)
+{   int i=0;
+	if (*s)
+	{    printf("%d", i);
+		_print_rev_recursion(s + 1);
+		i++;
+		_putchar(*s);
+
+	}
+}
+int main(void)
+{
+    char t[10]= "thibaut" ;
+    _print_rev_recursion(t);
+}
