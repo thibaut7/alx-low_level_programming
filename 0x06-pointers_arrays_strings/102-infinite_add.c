c0c76e176dcc2e21758665d8b0563c3c61b8f19e
thibaut <86150292+thibaut7@users.noreply.github.com>
2022-11-22 13:31:22 +0000

    Update 102-infinite_add.c

--- 0x06-pointers_arrays_strings/102-infinite_add.c
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -3,7 +3,7 @@
  * Auth: Brennan D Baraban
  */
 
-#include "holberton.h"
+#include "main.h"
 
 char *add_strings(char *n1, char *n2, char *r, int r_index);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
